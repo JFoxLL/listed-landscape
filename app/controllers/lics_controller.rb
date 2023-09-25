52833d89f56e8141731243a208dbd3f0fa4671a4
John Fox <john.fox@listedlandscape.com>
2023-09-25 12:28:59 +1000

    Added filting of index action table

--- app/controllers/lics_controller.rb
+++ app/controllers/lics_controller.rb
@@ -1,13 +1,24 @@
 class LicsController < ApplicationController
-
+    helper_method :sort_column, :sort_direction
+  
     def index
-        sort_by = params[:sort_by] || 'market_cap'
-        sort_order = params[:sort_order] || 'desc'
-        @lics = Lic.order("#{sort_by} #{sort_order}")
+      # Dropdown menu
+      @investment_focus_options = Lic.group(:investment_focus).order('count_id desc').count('id').keys
+      
+      # Filtering logic
+      if params[:investment_focus].present?
+        @lics = Lic.where(investment_focus: params[:investment_focus])
+      else
+        @lics = Lic.all
+      end
+      
+      # Sorting logic
+      @sort_column = params[:sort_by] || 'market_cap'
+      @sort_direction = params[:sort_order] || 'desc'
+      @lics = @lics.order(@sort_column => @sort_direction)
     end
-
+  
     def show
-        @lic = Lic.find_by!(slug: params[:id])        
+      @lic = Lic.find_by!(slug: params[:id])
     end
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+    def sortable(column, title = nil, current_sort_column, current_sort_direction)
+        title ||= column.titleize
+        css_class = (column == current_sort_column) ? "current #{current_sort_direction}" : nil
+        direction = (column == current_sort_column && current_sort_direction == "asc") ? "desc" : "asc"
+        
+        # Include the current investment_focus parameter, if any
+        link_params = { sort_by: column, sort_order: direction }
+        link_params[:investment_focus] = params[:investment_focus] if params[:investment_focus].present?
+        
+        link_to title, link_params, class: css_class
+    end
+    
 end
