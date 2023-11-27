39c99df4facf3222707227365412661ee1f34aa5
John Fox <john.fox@listedlandscape.com>
2023-11-27 15:32:25 +1000

    Added an Investment Focus filter to SPvNTA table

--- app/controllers/share_price_vs_ntas_controller.rb
+++ app/controllers/share_price_vs_ntas_controller.rb
@@ -2,7 +2,20 @@ class SharePriceVsNtasController < ApplicationController
     before_action :set_device_type
 
     def index
-        @lics = Lic.includes(:share_price_vs_nta).order(market_cap: :desc)
+        @investment_focus_options = Lic.group(:investment_focus).order('count_id desc').count('id').keys
+
+        if params[:investment_focus].present?
+            @lics = Lic.includes(:share_price_vs_nta).where(investment_focus: params[:investment_focus])
+        else
+            @lics = Lic.includes(:share_price_vs_nta).order(market_cap: :desc)
+        end
+
+        respond_to do |format|
+            format.html
+            format.turbo_stream do
+              render turbo_stream: turbo_stream.update('share_price_vs_ntas_index_table', partial: 'share_price_vs_ntas_index_table', locals: { lics: @lics })
+            end
+        end
     end
 
 end
