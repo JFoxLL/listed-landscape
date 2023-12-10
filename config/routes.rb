38fb8f7e6855eb1af0fd561a36b0c0cf6cfdb9f8
John Fox <john.fox@listedlandscape.com>
2023-12-11 08:09:02 +1000

    Fixing home page duplicate url

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-
+  
+  # Home page
   root "lics#index"
 
-  get "listed-investment-companies" => "lics#index"
+  # Lic show pages
   get "listed-investment-companies/:id" => "lics#show", as: "listed-investment-company"
 
+  # Dedicated summary pages
   get "share-price-vs-nta" => "share_price_vs_ntas#index"
   get "performance" => "performances#index"
   get "dividend-yields" => "dividend_yields#index"
+  
+  # Redirects
+  get '/listed-investment-companies', to: redirect('/')   # This was an only page, no longer in use. 
 
 end
