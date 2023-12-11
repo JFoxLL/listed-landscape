05886fc323ab18bd0a9eb90a0ec7edf0ad958cd5
John Fox <john.fox@listedlandscape.com>
2023-12-11 11:17:56 +1000

    Improving home page seo details

---
+++ app/javascript/controllers/common_questions_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["question", "answer"];
+
+  toggle(event) {
+    const question = event.currentTarget;
+    const answer = question.nextElementSibling;
+
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+      question.querySelector(".common-questions-toggle-icon").textContent = "+";
+    } else {
+      answer.style.display = "block";
+      question.querySelector(".common-questions-toggle-icon").textContent = "-";
+    }
+  }
+}
