6d1f2b88c968aa10a095503af04c0e12157b5768
John Fox <john.fox@listedlandscape.com>
2023-12-14 09:12:27 +1000

    Setting up NumberShares resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_13_000956) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_13_230405) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -170,6 +170,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_13_000956) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "lic_id"
+  end
+
+  create_table "number_shares", force: :cascade do |t|
+    t.string "lic_ticker"
+    t.string "lic_name"
+    t.integer "year"
+    t.bigint "number_shares"
+    t.integer "lic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "performances", force: :cascade do |t|
