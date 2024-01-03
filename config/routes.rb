ae8252a702e716a22ba91c34499a667513231e66
John Fox <john.fox@listedlandscape.com>
2024-01-04 09:00:55 +1000

    Setting up Div Histories

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
   root "lics#index"
 
   # Lic show pages
-  get "listed-investment-companies/:id" => "lics#show", as: "listed-investment-company"
+  get "listed-investment-companies/:id" => "lics#show", as: "listed_investment_company"
 
   # Dedicated summary pages
   get "share-price-vs-nta" => "share_price_vs_ntas#index"
   get "performance" => "performances#index"
   get "dividend-yields" => "dividend_yields#index"
+
+  # Level 3 SEO Pages
+  get "listed-investment-companies/:id/dividend-history" => "dividend_histories#show", as: "lics_dividend_histories"
   
   # Redirects
   get '/listed-investment-companies', to: redirect('/')   # This was an only page, no longer in use. 
