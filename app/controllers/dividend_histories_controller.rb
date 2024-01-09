87fd444b920eb759403d830eaaec7e035f6b524f
John Fox <john.fox@listedlandscape.com>
2024-01-09 12:49:05 +1000

    Div History Page Building

--- app/controllers/dividend_histories_controller.rb
+++ app/controllers/dividend_histories_controller.rb
@@ -2,6 +2,9 @@ class DividendHistoriesController < ApplicationController
 
     def show
         @lic = Lic.find_by!(slug: params[:id])
+
+        # Dividend History Chart
+        @dividend_history_view_type = params[:dividend_history_view] || "annualised"
     end
 
 end
