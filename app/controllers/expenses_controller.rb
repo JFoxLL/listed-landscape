43ccc80959147502f04ce5cde1d0964c3fb200dc
John Fox <john.fox@listedlandscape.com>
2024-01-16 10:46:52 +1000

    Expenses L3 SEO page content population

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,6 +2,17 @@ class ExpensesController < ApplicationController
 
     def show
         @lic = Lic.find_by!(slug: params[:id])
+
+        # Cost Indicator Chart
+        @cost_indicator_view_type = params[:cost_indicator_view] || "excluding_performance_fees"
+
+        # Costs Incurred Chart
+        @costs_incurred_view_type = params[:costs_incurred_view] || "total"
+
+        respond_to do |format|
+            format.html
+            format.turbo_stream
+        end
     end
     
 end
