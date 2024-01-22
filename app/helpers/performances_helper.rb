fffe0be2255805e6a5d1c6c851b0e66b30328687
John Fox <john.fox@listedlandscape.com>
2024-01-22 11:51:53 +1000

    Tweaking Performance Table Colour Coding

--- app/helpers/performances_helper.rb
+++ app/helpers/performances_helper.rb
@@ -3,10 +3,11 @@ module PerformancesHelper
     def performance_table_colour_shading(value)
 
         if value.is_a?(Numeric)
+            value_rounded = value.round(0)
 
-            if value < 0
+            if value_rounded < 0
                 "negative-performance"
-            elsif value > 6
+            elsif value_rounded > 6
                 "strong-performance"
             else
                 "neutral-performance"
